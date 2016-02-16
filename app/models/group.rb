af1d65f1aed483d6bcefdca9bf1b4288a5f2e50c
zeichenjoyce <zeichenjoyce@gmail.com>
2016-02-16 17:55:10 +0800

    可以在 group 的 show 頁面顯示所擁有的 post
    可以在 group 的 show 頁面顯示所擁有的 post

---
+++ app/models/group.rb
@@ -0,0 +1,4 @@
+class Group < ActiveRecord::Base
+	validates :title, presence: true
+	has_many :posts
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'groups#index' 
+  resources :groups do
+    resources :posts
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
