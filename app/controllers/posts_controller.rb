4a35835b003a5c0fee74f499ac17fa3122549af5
zeichenjoyce <zeichenjoyce@gmail.com>
2016-02-16 23:50:27 +0800

    可以修改並更新 post ( action edit, update )
    可以修改並更新 post ( action edit, update )

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,40 @@
 class PostsController < ApplicationController
+	def new
+		@group = Group.find(params[:group_id])
+		@post = @group.posts.new
+	end
+
+	def edit 
+    @group =Group.find(params[:group_id])
+	@post =@group.posts.find(params[:id])
+	end
+	
+	def create 
+	@group = Group.find(params[:group_id])
+	@post = @group.posts.build(post_params)
+
+	if @post.save
+		redirect_to group_path(@group), notice:"新增文章成功"
+	else
+		render :new
+	end
+	end
+
+	def update
+	@group =Group.find(params[:group_id])
+	@post =@group.posts.find(params[:id])
+	 if @post.update(post_params)
+		redirect_to group_path(@group), notice:"文章修改成功"
+	else
+		render :edit
+	end
+
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:content)
+	end
+
+
 end
